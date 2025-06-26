bd07fdec38bf0004ed51a89d541328ee75d312d3
newest-j <ibukunagbaoye@gmail.com>
2025-06-26 09:53:06 +0100

    jsonserver setup

--- src/product/product.ts
+++ src/product/product.ts
@@ -1,10 +1,24 @@
 export interface Product {
+  category: string;
+  icon: string;
   id: number;
   name: string;
-  category: string;
-  quantity: number;
   price: number;
-  supplier: string;
+  quantity: number;
   status: string;
-  icon: string;
+  supplier: string;
+}
+
+export interface ProductStatus {
+  total_product: number;
+  in_stock: number;
+  low_stock: number;
+  out_of_stock: number;
+  total_categories: number;
+  active_categories: number;
+}
+
+export interface ProductResponse {
+  items: Product[];
+  Status: ProductStatus;
 }
